396b2147881944c014e59098051658e0bb352ed4
albertozelaya5 <albertolaniman@gmail.com>
2025-07-19 21:27:59 -0600

    fix: change to https to request the API

--- src/useMovies.js
+++ src/useMovies.js
@@ -17,7 +17,7 @@ export function useMovies(query, callBack) {
         try {
           setIsLoading(true);
           setError("");
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
+          const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
 
           if (!res.ok) throw new Error("Something went wrong");
 
